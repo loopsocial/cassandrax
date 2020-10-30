c5e7eb192a3860420920175bc14b46f18cbe69cc
Thiago Dias <tdtadeu@gmail.com>
2020-10-30 19:21:07 -0300

    Test

--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule Cassandrax.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {Cassandrax, []}
     ]
   end
 
