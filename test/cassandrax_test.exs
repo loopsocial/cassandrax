32800d3192c6dbb89c2bef3fc11801693efaac5d
Thiago Dias <tdtadeu@gmail.com>
2020-11-13 14:38:08 -0300

    Added tests for ensure_cluster_config!/2

--- test/cassandrax_test.exs
+++ test/cassandrax_test.exs
@@ -2,7 +2,17 @@ defmodule CassandraxTest do
   use ExUnit.Case
   doctest Cassandrax
 
-  test "greets the world" do
-    assert Cassandrax.hello() == :world
+  describe "ensure_cluster_config!/2" do
+    test "raises an error when config is nil" do
+      assert_raise(Cassandrax.ClusterConfigError, ~r/Expected to find keyword configs/, fn ->
+        Cassandrax.ensure_cluster_config!(nil, %{})
+      end)
+    end
+
+    test "raises an error when config is []" do
+      assert_raise(Cassandrax.ClusterConfigError, ~r/Expected to find keyword configs/, fn ->
+        Cassandrax.ensure_cluster_config!([], %{})
+      end)
+    end
   end
 end
