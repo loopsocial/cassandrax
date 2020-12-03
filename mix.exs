072e7c338f32d2c90ae094cdb2cd826ba14026d6
Thiago Dias <tdtadeu@gmail.com>
2020-12-03 12:04:07 -0300

    Bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Cassandrax.MixProject do
   use Mix.Project
 
-  @version "0.0.1"
+  @version "0.0.2"
   @url "https://github.com/loopsocial/cassandrax"
   @maintainers ["Thiago Dias", "Doga Tuncay"]
 
