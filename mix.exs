01b321292e3468e2700985ca72c990d2895140f0
Doga Tuncay <doga@Dogas-MacBook-Pro.local>
2024-04-11 14:13:21 -0400

    prepare version update to 0.3.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Cassandrax.MixProject do
   use Mix.Project
 
-  @version "0.2.0"
+  @version "0.3.0"
   @url "https://github.com/loopsocial/cassandrax"
   @maintainers ["Thiago Dias", "Doga Tuncay"]
 
